f4a907205163d95d7abfeb5b9abf14129760ddbd
Karoly Molnar <karoly.molnar@macifique.com>
2021-12-15 23:23:18 +0100

    practice commits

---
+++ echo/src/main.rs
@@ -0,0 +1,31 @@
+use clap::{App, Arg};
+
+fn main() {
+    // println!("{:?}", std::env::args());
+    let matches = App::new("echo")
+        .version("0.1.0")
+        .author("Karoly Molnar")
+        .about("Rust echo")
+        .arg(
+            Arg::with_name("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .min_values(1),
+        )
+        .arg(
+            Arg::with_name("omit_newline")
+                .short("n")
+                .help("Do not print newline")
+                .takes_value(false),
+        )
+        .get_matches();
+    let text = matches.values_of_lossy("text").unwrap();
+    let omit_nl = matches.is_present("omit_newline");
+    let mut ending = "\n";
+    if omit_nl {
+        ending = "";
+    }
+    print!("{}", text.join(" ") + ending);
+
+}
